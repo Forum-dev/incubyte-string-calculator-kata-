9c423cd0e386575357832637c27958c9c782b59f
Foram <foramthakral.tech@gmail.com>
2025-10-15 23:51:45 +0530

    TDD Step 6: Test and impl for custom delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,12 +1,24 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    parse_numbers(numbers).reduce(0, :+)
+    
+    delimiter = /[,|\n]/
+    if numbers.start_with?('//')
+      delimiter, numbers = parse_custom_delimiter(numbers)
+    end
+
+    parse_numbers(numbers, delimiter).reduce(0, :+)
   end
 
   private
 
-  def parse_numbers(numbers)
-    numbers.gsub("\n", ",").split(',').map(&:to_i)
+  def parse_custom_delimiter(numbers)
+    parts = numbers.split("\n", 2)
+    delimiter = parts[0][2..]
+    [delimiter, parts[1]]
+  end
+
+  def parse_numbers(numbers, delimiter = /[,|\n]/)
+    numbers.split(delimiter).map(&:to_i)
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -25,5 +25,9 @@ RSpec.describe StringCalculator do
     it 'handles newlines between numbers' do
       expect(subject.add("1\n2,3")).to eq(6)
     end
+
+    it 'supports single custom delimiter' do
+      expect(subject.add("//;\n1;2")).to eq(3)
+    end
   end
 end
